ed14514a9983ca94d90761eacf2f6ab90b964a68
Karolina Kopacz <karolina.kopacz94@gmail.com>
2022-08-24 17:38:17 +0200

    TRAN-67: Added simple login page skeleton & landing page (#10)
    * TRAN-67: Added simple login page skeleton & landing page
    
    * TRAN-67: Resolved conflicts and fix eslint errors

---
+++ src/pages/signIn/SignInPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SignInPage() {
+  return (
+    <div className="flex h-screen w-screen bg-transit-grey" data-testid="sign-in-page">
+      <div className="sm:w-1/2 bg-hero-image bg-no-repeat bg-cover" />
+      <div className="w-full sm:w-1/2" />
+    </div>
+  );
+}
+
+export default SignInPage;
