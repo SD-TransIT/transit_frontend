ec441e0e76e14f0c0a95b69c0056f728cd7ad52b
Karolina Kopacz <karolina.kopacz94@gmail.com>
2022-08-25 16:00:36 +0200

    TRAN-67: Fix hero image

--- src/pages/signIn/SignInPage.tsx
+++ src/pages/signIn/SignInPage.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function SignInPage() {
   return (
     <div className="flex h-screen w-screen bg-transit-grey" data-testid="sign-in-page">
-      <div className="sm:w-1/2 bg-hero-image bg-no-repeat bg-cover" />
+      <div className="sm:w-1/2 bg-hero-image bg-no-repeat bg-cover bg-center" />
       <div className="w-full sm:w-1/2" />
     </div>
   );
