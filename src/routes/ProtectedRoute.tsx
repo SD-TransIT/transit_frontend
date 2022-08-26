f5c7faede3c7d54f82eb1d57dea02341b101aa93
sniedzielski <sniedzielski@soldevelo.com>
2022-08-26 11:33:09 +0200

    TRAN-66: fixed another conflicts

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-import { Navigate } from 'react-router-dom';
-import React from 'react';
-import isAuthenticatedProperly from '../utils/authHelper';
-=======
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import isAuthenticated from '../utils/authHelper';
->>>>>>> d0a59ad5d32178430a9580eda2f8c5b917a3e76b
 import Header from '../components/header/Header';
 
 export type ProtectedRouteProps = {
@@ -15,11 +9,7 @@ export type ProtectedRouteProps = {
 };
 
 export default function ProtectedRoute({ authenticationPath, outlet }: ProtectedRouteProps) {
-<<<<<<< HEAD
-  if (isAuthenticatedProperly()) {
-=======
   if (isAuthenticated()) {
->>>>>>> d0a59ad5d32178430a9580eda2f8c5b917a3e76b
     return (
       <>
         <Header />
