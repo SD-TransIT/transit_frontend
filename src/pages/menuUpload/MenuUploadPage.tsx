ceed76dff9ee9c703298b6f00d0d08a6c1a5496a
Karolina Kopacz <karolina.kopacz94@gmail.com>
2022-09-02 11:46:08 +0200

    TRAN-75: Put css style to globas.css

--- src/pages/menuUpload/MenuUploadPage.tsx
+++ src/pages/menuUpload/MenuUploadPage.tsx
@@ -7,8 +7,6 @@ import { getManualUploadFormsRequest } from '../../redux/actions/menuUpload/menu
 import { RootState } from '../../redux/reducers/rootReducer';
 import PageHeader from '../types';
 
-const menuTileStyle = 'flex justify-start bg-transit-white px-6 py-4 rounded border border-transit-grey hover:border-x hover:border-transit-green-dark font-normal font-transit-black hover:text-transit-green-dark';
-
 function ManualUploadPage() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -31,7 +29,7 @@ function ManualUploadPage() {
     <PageBody title={PageHeader.manual_upload}>
       <div className="grid grid-cols-2 gap-x-8 gap-y-4">
         {manualUploadForms?.map((tile: IManualUploadFormsType) => (
-          <button type="button" key={tile.label} className={menuTileStyle} onClick={() => callback(tile.label)}>
+          <button type="button" key={tile.label} className="menu-tile" onClick={() => callback(tile.label)}>
             <p>{tile.name}</p>
           </button>
         ))}
