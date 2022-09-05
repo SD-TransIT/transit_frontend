71e602d0857baeb175c533909dc7ac11e30da339
sniedzielski <sniedzielski@soldevelo.com>
2022-09-05 19:15:42 +0200

    TRAN-84: fixed import

--- src/redux/sagas/rootSaga.tsx
+++ src/redux/sagas/rootSaga.tsx
@@ -2,7 +2,7 @@ import { all, fork } from 'redux-saga/effects';
 import tokenSaga from './tokenSaga';
 import customerTypeSaga from './customerTypeSaga';
 import manualUploadFormsSaga from './manualUploadFormsSaga';
-import driverSaga from './driverTypeSaga';
+import driverSaga from './driverSaga';
 
 export default function* rootSaga() {
   yield all([
