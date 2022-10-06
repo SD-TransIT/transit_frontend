feec538aa6d81b428cd58ff9f9505441c3711ac1
Karolina Kopacz <karolina.kopacz94@gmail.com>
2022-10-06 11:40:36 +0200

    TRAN-149: Added language chooser to main page and replace already existing one on sign in page

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import HeaderMenu from 'components/header/HeaderMenu';
 import Logo from 'components/logo/Logo';
 import Logout from 'components/logout/Logout';
+import LanguageSelect from 'components/shared/LanguageSelect';
 
 function Header() {
   return (
@@ -13,7 +14,8 @@ function Header() {
       <div className="w-3/5">
         <HeaderMenu />
       </div>
-      <div className="w-1/5">
+      <div className="flex flex-row w-1/5 gap-7 justify-end">
+        <LanguageSelect />
         <Logout />
       </div>
     </div>
--- src/pages/signIn/SignInPage.tsx
+++ src/pages/signIn/SignInPage.tsx
@@ -1,36 +1,16 @@
 import React from 'react';
 
-import { useCookies } from 'react-cookie';
-
 import SignInForm from 'components/forms/signInForm';
-import SimpleSelect from 'components/shared/SimpleSelect';
+import LanguageSelect from 'components/shared/LanguageSelect';
 
 export default function SignInPage() {
-  const [langCookies, setLangCookie] = useCookies(['language']);
-
-  const options = [
-    { value: 'en', label: 'English' },
-    { value: 'fr', label: 'French' },
-    { value: 'pt', label: 'Portugese' },
-  ];
-
-  const onLocaleChange = (event: any) => {
-    setLangCookie('language', { value: event.value, label: event.label }, { path: '/', sameSite: true });
-  };
-
   return (
     <div className="flex h-screen w-screen bg-transit-grey" data-testid="sign-in-page">
       <div className="sm:w-1/2 bg-hero-image bg-no-repeat bg-cover" />
       <div className="w-full sm:w-1/2">
         <div className="flex flex-col h-screen">
           <div className="flex justify-end w-full pr-10 pt-10">
-            <SimpleSelect
-              value={langCookies.language?.label}
-              onChange={onLocaleChange}
-              options={options}
-              className="rounded w-1/5"
-              placeholder={langCookies.language?.label}
-            />
+            <LanguageSelect />
           </div>
           <SignInForm />
         </div>
