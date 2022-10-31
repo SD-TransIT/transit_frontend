81e080a1903619a88c434a12f91172586ed6efeb
Karolina Kopacz <karolina.kopacz94@gmail.com>
2022-10-31 09:24:32 +0100

    Hotfix: Changed logo (#96)

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-import logo from 'images/logo.svg';
+import transitName from 'images/transitName.jpg';
+import truck from 'images/truck.jpg';
 
 function Logo() {
   return (
-    <div className="flex h-full items-center">
-      <img src={logo} alt="logo" width="150px" height="90px" />
+    <div className="flex h-full items-center gap-2">
+      <img src={truck} alt="logo" width="33px" height="24px" />
+      <img src={transitName} alt="logo" width="65px" height="13px" />
     </div>
   );
 }
